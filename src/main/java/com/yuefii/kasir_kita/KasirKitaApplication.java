f30f7c8ea9b8a5dd500b8f1dc887ef6af6be8f57
Yuefii <muffidahmadiawan@gmail.com>
2025-01-02 14:31:40 +0700

    initialize commit

---
+++ src/main/java/com/yuefii/kasir_kita/KasirKitaApplication.java
@@ -0,0 +1,13 @@
+package com.yuefii.kasir_kita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KasirKitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KasirKitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuefii/kasir_kita/KasirKitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuefii.kasir_kita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KasirKitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
