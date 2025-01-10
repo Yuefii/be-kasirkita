809601918d400666b020ba6f93f14f1ee26cb6ce
Yuefii <muffidahmadiawan31@gmail.com>
2025-01-10 16:28:09 +0700

    chore: adding cors origin

--- src/main/java/com/yuefii/kasir_kita/WebConfiguration.java
+++ src/main/java/com/yuefii/kasir_kita/WebConfiguration.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.yuefii.kasir_kita.resolver.UserArgsResolver;
@@ -19,5 +20,14 @@ public class WebConfiguration implements WebMvcConfigurer {
   public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
     WebMvcConfigurer.super.addArgumentResolvers(resolvers);
     resolvers.add(userArgsResolver);
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("http://localhost:3000")
+        .allowedMethods("GET", "POST", "PATCH", "DELETE")
+        .allowedHeaders("*")
+        .allowCredentials(true);
   }
 }
